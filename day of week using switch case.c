fafa4b4f6d6a52fd5107cab35c4e890510de829d
Sainithin Motupalli <93912388+sainithin0103@users.noreply.github.com>
2021-12-03 10:02:18 +0530

    Add files via upload

---
+++ day of week using switch case.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+   int main()
+{
+   int week;
+    printf("Enter week number: ");
+    scanf("%d", &week);
+    
+    switch(week)
+{
+
+    case 1:
+	printf("MONDAY");
+	break;
+	
+	case 2:
+	printf("TUESDAY");
+	break;
+	
+	case 3:
+	printf("WEDNESDAY");
+	break;
+	
+	case 4:
+	printf("THURSDAY");
+	break;
+	
+	case 5:
+	printf("FRIDAY");
+	break;
+	
+	case 6:
+	printf("SATURDAY");
+	break;
+	
+	case 7:
+	printf("SUNDAY");
+	break;
+}
+}
